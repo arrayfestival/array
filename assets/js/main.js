e57a73c1d73895aa6801c923d4baeb20ecdf97df
Gareth Jones <gjones@torpedogroup.com>
2020-06-04 13:22:26 +0100

    New Type 8 card style for BLM link, animate it into place first.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,18 +11,20 @@ $(function () {
   // then main colour cards, then main background.
   // Finally animate out type 7 grey cards to reveal more of the background
   if ($('html').hasClass('js')) {
-    gsap.to('.logo-main', {duration: 1.5, opacity: 1, delay: .5})
+    var $blmCard = $('.card--type-8')
+    gsap.to($blmCard, {duration: 1.5, opacity: 1, delay: .5})
+    gsap.to('.logo-main', {duration: 1.5, opacity: 1, delay: 2.5})
     var $introCards = $('.card--type-1')
     $introCards.each(function (index, elem) {
-      gsap.to(elem, {duration: .5, opacity: 1, delay: (index / 4) + 1.5})
+      gsap.to(elem, {duration: .5, opacity: 1, delay: (index / 4) + 3.5})
     })
     var $timeCards = $('.card--time')
     $timeCards.each(function (index, elem) {
-      gsap.to(elem, {duration: .5, opacity: 1, delay: index + 2.5})
+      gsap.to(elem, {duration: .5, opacity: 1, delay: index + 4.5})
     })
-    var $remainingCards = $('.card:not(.card--type-1):not(.card--type-6):not(.card--type-7):not(.card--time), .logo, .artist-image')
+    var $remainingCards = $('.card:not(.card--type-1):not(.card--type-6):not(.card--type-7):not(.card--type-8):not(.card--time), .logo, .artist-image')
     $remainingCards.each(function (index, elem) {
-      var delay = 3 + (Math.random() * 2)
+      var delay = 3 + (Math.random() * 4)
       var direction = Math.floor(Math.random() * 4)
       var sign = ''
       if (direction === 1 || direction === 2) {
@@ -35,10 +37,10 @@ $(function () {
       }
       gsap.to(elem, {duration: 1, opacity: 1, delay: delay})
     })
-    gsap.to($('.container-background'), {duration: 1, opacity: 1, delay: 6})
+    gsap.to($('.container-background'), {duration: 1, opacity: 1, delay: 8})
     var $gapCards = $('.card--type-7')
     $gapCards.each(function (index, elem) {
-      var delay = 7 + (Math.random() * 2)
+      var delay = 9 + (Math.random() * 2)
       var direction = Math.floor(Math.random() * 4)
       var $gapCard = $(elem)
       $gapCard.css('transform-origin', 'top left')
